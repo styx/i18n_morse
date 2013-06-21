1c745fcb1709cb4a09957e62643bd53b061ff52c
Mikhail S. Pobolovets <styx.mp@gmail.com>
2013-06-21 09:56:36 +0300

    Fixed codetable loading.
    Signed-off-by: Mikhail S. Pobolovets <styx.mp@gmail.com>

--- lib/morse/codetables.rb
+++ lib/morse/codetables.rb
@@ -1,8 +1,10 @@
 # coding: utf-8
 
+require 'yaml'
+
 module CodeTables
   gem_root = Gem.loaded_specs['i18n_morse'].full_gem_path
 
   CODE_TABLES = {}
-  Dir["#{gem_root}/codetables/*.yml"].each {|f| CODE_TABLES.merge(YAML.load(File.read(f))) }
+  Dir["#{gem_root}/codetables/*.yml"].each {|f| CODE_TABLES.merge!(YAML.load(File.read(f))) }
 end
